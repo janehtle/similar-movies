4793eba5a22030b46fc12d1737a8ace42b669c85
Jane Le <janehtle@gmail.com>
2025-09-09 23:09:22 -0400

    changes to code

--- public/app.js
+++ public/app.js
@@ -10,7 +10,8 @@ document.addEventListener("DOMContentLoaded", () => { //make sure all contents l
     const movieList = document.getElementById("movieList");
 
     searchBtn.addEventListener("click", () => { //add functionality to search button
-        const movieName = movieNameBar.value.trim(); //remove any white spaces on the ends
+        const movieName = movieNameBar.value.trim(); //remove any white spaces on the ends, this variable's value will be the value 
+                                                     //pulled from input field, aka name of movie user inputted
         console.log("Movie:", movieName); //checking to make sure value from input bar is accessed and will be displayed in console
     });
 });
--- src/tmdb.js
+++ src/tmdb.js
@@ -1,25 +1,27 @@
 const request = require("request"); //load it into module
 
 function similarMovies(movieName, callback) {
-    const apiKey = process.env.tmdbAPI; //access API key from env file, NO HARD CODING API KEY!
+    const apiKey = process.env.TMDB_API; //access API key from env file, NO HARD CODING API KEY!
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(movieName)}`;
-    //
+    //can see where no hard coding of API key is present in url, its stored in a variable instead!
 
-    request({ url: url, json: true }, (err, res, body) => {
-        if (err) return callback(err);
+    request({ url: url, json: true }, (error, res, body) => { //sending GET request
+        if (error) return callback(error); //throw an error if there are any complications
 
         const movie = body.results && body.results[0];
-        if (!movie) return callback(new Error("Movie not found :("));
+        if (!movie) return callback(new Error("Movie not found :(")); 
+        //if the movie doesn't exist, then throw a new error with custom string
+        
+        //to identify similar movies, find the unique movie id of the movie similarities should be based on
+        const movieId = movie.id; //every movie has a unique id, get that id
+        const similarUrl = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${apiKey}`; //url for similar feature on tmdb site
 
-        const movieId = movie.id;
-        const similarUrl = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${apiKey}`;
-
-        request({ url: similarUrl, json: true }, (err, res, body) => {
-            if (err) return callback(err);
+        request({ url: similarUrl, json: true }, (error, res, body) => {
+            if (error) return callback(error);
             callback(null, body);  
         });
     });
 }
 
-module.exports = similarMovies;
+module.exports = {similarMovies};
